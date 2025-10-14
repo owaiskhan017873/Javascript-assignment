e950e1815207780f6ca5b75daa35f3fc62f994cf
owaiskhan017873 <okahw.pk@gmail.com>
2025-10-14 18:14:01 +0500

    Add files via upload

---
+++ Assignment 1/Script.js
@@ -0,0 +1,36 @@
+           
+           ///alert 
+window.alert("heloo" )
+
+              //variable
+var myNumber = 44;  
+var myBoolean = true;   
+var myString = "hello";  
+var myNull = null;  
+var mySymbol = Symbol("id");
+var mybignumber = 2938484949449494949493939399n;
+
+console.log (typeof(myNumber))
+console.log (typeof(myboolean))
+console.log (typeof(myString))
+console.log (typeof(myNull))
+console.log (typeof(mySymbol))
+console.log (typeof(mybignumber))
+
+
+
+let o ={
+  name : "muhammad",
+  age: 48495
+}
+console.log(o);
+o.role=93393
+console.log(o)
+
+
+
+let sum = 4 + 8;
+console.log(sum);
+
+let sub = 10 - 5;
+console.log(sub);
