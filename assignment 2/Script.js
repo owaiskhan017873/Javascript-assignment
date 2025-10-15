9947df883a8a3beba5a589fd835669fce4792f36
owaiskhan017873 <okahw.pk@gmail.com>
2025-10-15 13:07:54 +0500

    Add files via upload

---
+++ assignment 2/Script.js
@@ -0,0 +1,52 @@
+let v1 = 10
+let v2 = 20 
+let v3 = 30
+let total = v1 + v2 + v3 
+console.log(total)
+
+
+//legal 
+// var myName = 
+// var name1 =
+// var $price =
+// var _count = 
+// totalamount1 = 
+ 
+//ilegal
+//var 1name =
+//var first-last =
+// @count = 
+// my name =
+
+
+document.write(`<h1>Java script</h1>`)
+
+
+var num1 = 14
+var num2 = 7
+var ans = num1 % num2 
+console.log(ans)
+
+
+
+var ticket = 600
+var people = 5
+console.log(ticket*people)
+
+
+
+
+let item1 = 650
+let quantity1 = 3 
+let item2 = 150 
+let quantity2 = 5 
+let charges = 100 
+let totalmoney = item1 * quantity1 + item2 * quantity2 + charges
+
+console.log(totalmoney)
+
+
+
+
+
+
