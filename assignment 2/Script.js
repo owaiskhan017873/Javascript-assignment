75467cd6d7ddac6876e10588174a96e5f8f158ea
owaiskhan017873 <okahw.pk@gmail.com>
2025-10-22 14:12:54 +0500

    Add files via upload

---
+++ assignment 2/Script.js
@@ -0,0 +1,42 @@
+
+    // 1. Declare 3 variables in one statement
+    let a = 10, b = 20, c = 30;
+    
+    
+    
+    
+
+    // 2. Declare 5 legal and 5 illegal variable names
+    // ✅ Legal variable names
+    let name;
+    let $price;
+    let _age;
+    let userName;
+    let my1stVariable;
+    
+    
+    
+    
+
+    // ❌ Illegal variable names (commented out to avoid error)
+    // let 1name;
+    // let my-name;
+    // let var;
+    // let user name;
+    // let @value;
+    
+    
+    
+    
+    
+
+    // 3. Display rules in browser
+    document.write("<b>Variable names can only contain letters, numbers, $ and _ .</b><br>");
+    document.write("For example: <b>$my_1stVariable</b><br><br>");
+
+    document.write("<b>Variables must begin with a letter, $ or _ .</b><br>");
+    document.write("For example: <b>$name</b>, <b>_name</b> or <b>name</b><br><br>");
+
+    document.write("<b>Variable names are case sensitive.</b><br><br>");
+
+    document.write("<b>Variable names should not be JS keywords.</b><br>");
